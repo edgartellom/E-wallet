f552955db027bae7162a7169fb75e167dde244bc
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2023-02-02 23:19:03 -0500

    added body and footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import { Footer } from './components/footer/footer.component'
 import { Navbar } from './components/head/Navbar.component'
+import { Home } from './components/pages/home.page'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
