b9729c512633d86321cd2318386dbabcc9f05919
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2023-02-03 18:21:36 -0500

    added banner in page home as other component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,9 @@
+import Banner from './Banner';
+
 const HomePage = ()=>{
     return (<>
+
+        <Banner></Banner>
         <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
