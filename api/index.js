1756ebcb389c57914eb8175a8c0c9b755546a396
edgartellom <edgar.tello@tecsup.edu.pe>
2023-02-05 23:33:11 -0300

    config cors api/index

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
 // const port = 3000;
 
 // //ruta del frontend
