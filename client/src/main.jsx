a64613168b41e00aad6c93fec04ad37e22457cd8
edgartellom <edgar.tello@tecsup.edu.pe>
2023-02-09 22:47:00 -0300

    config deploy front

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { store } from './redux/store';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://sphoenix-production.up.railway.app/';
+axios.defaults.baseURL = import.meta.env.VITE_APP_API || 'http://localhost:3001';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
