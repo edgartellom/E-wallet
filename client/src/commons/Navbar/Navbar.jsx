d9314e14750cac909a9405d3dc988c56d0e134e1
edgartellom <edgar.tello@tecsup.edu.pe>
2023-02-11 19:19:08 -0300

    redeploy attemp 4

--- client/src/commons/Navbar/Navbar.jsx
+++ client/src/commons/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import Cart from "../Cart/Cart"
-import SearchBar from "../searchbar/SearchBar"
+import SearchBar from "../Searchbar/SearchBar"
 
 
 const Navbar = () => {
