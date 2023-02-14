11879aa31212bd434bf29d6fe5dfdb361cc1d049
diegobonzo <diego-bonzo@hotmail.com>
2023-02-14 20:08:52 -0300

    atAbout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,27 @@
-import './App.css';
-import Navbar from './commons/Navbar/Navbar';
-import HomePage from './pages/Home/Home';
-import Details from './pages/Details/Details';
-import Footer from './commons/Footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CreatePhones from './components/Create/CreatePhones';
-import ProductsPage from './pages/Products/ProductsPage';
+import "./App.css";
+import Navbar from "./commons/Navbar/Navbar";
+import HomePage from "./pages/Home/Home";
+import Details from "./pages/Details/Details";
+import Footer from "./commons/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreatePhones from "./components/Create/CreatePhones";
+import ProductsPage from "./pages/Products/ProductsPage";
+import About from "./pages/About/About";
 
-
-  
 function App() {
-
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route exact path='/' element={<HomePage />} /> 
-        <Route path='/phone/:id' element={<Details />} />
-        <Route path='/products' element={<ProductsPage />}/>
-        <Route path='/phones' element={<CreatePhones />} />
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/phone/:id" element={<Details />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/phones" element={<CreatePhones />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-      <Footer/>
-      </BrowserRouter>
-  )
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
