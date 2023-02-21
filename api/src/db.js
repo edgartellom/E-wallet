49cb679bea990258c2c3cc46b164470a51faf9d4
edgartellom <edgar.tello@tecsup.edu.pe>
2023-02-21 12:33:37 -0300

    change to DB_DEPLOY

--- api/src/db.js
+++ api/src/db.js
@@ -4,18 +4,18 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/phoenix`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/phoenix`,
+//   {
+//     logging: false,
+//     native: false,
+//   }
+// );
 
-// const sequelize = new Sequelize(DB_DEPLOY, {
-//   logging: false,
-//   native: false,
-// });
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false,
+  native: false,
+});
 
 const basename = path.basename(__filename);
 
@@ -55,24 +55,6 @@ const Phone_Category = sequelize.define(
 );
 Phone.belongsToMany(Category, { through: Phone_Category });
 Category.belongsToMany(Phone, { through: Phone_Category }); //se utiliza para crear una asociación de muchos a muchos entre dos tablas
-
-const Phone_Order = sequelize.define(
-  "phone_order",
-  {},
-  { timestamps: false, freezeTableName: true }
-);
-Phone.belongsToMany(Order, { through: Phone_Order });
-Order.belongsToMany(Phone, { through: Phone_Order });
-
-// esta es la relacion de user a carrito
-const User_cart = sequelize.define(
-  "user_cart",
-  {},
-  { timestamps: false, freezeTableName: true }
-);
-User.belongsToMany(Cart, { through: User_cart });
-Cart.belongsToMany(User, { through: User_cart });
-//aqui las demasrelaciones
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
