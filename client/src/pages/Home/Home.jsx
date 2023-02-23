7890b9d798bd6ebe6e0213dfa54d5841db9063b9
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2023-02-23 17:28:16 -0500

    fixes 6

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import PhoneHome from "../../commons/phoneHome/PhoneHome";
+import Carrusel from "../../commons/carrusel/Carrusel";
+import Notification from "../../components/samples/notifications.sample";
+
+const Home = () => {
+  return (
+    <>
+      <section className="py-5">
+        <div>
+          <Carrusel />
+          <div>
+            <PhoneHome />
+          </div>
+          <Notification/>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
