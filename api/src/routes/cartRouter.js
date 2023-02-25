79df8742b70378d8fea91890133555cb915a1ed2
diegobonzo <diego-bonzo@hotmail.com>
2023-02-25 20:00:01 -0300

    reducercartsuser

--- api/src/routes/cartRouter.js
+++ api/src/routes/cartRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { Cart } = require("../db");
 
 const router = Router();
+
 router.get("/", async (req, res, next) => {
   try {
     const allCarts = await Cart.findAll();
---
+++ client/src/redux/slices/userCartsSlice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchUserCarts = createAsyncThunk(
+  'carts/fetchUserCarts',
+  async (userId, thunkAPI) => {
+    try {
+      const response = await axios.get(`/api/carts?userId=${userId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const initialState = {
+  userCarts: [],
+  status: 'idle',
+  error: null,
+};
+
+export const cartsSlice = createSlice({
+  name: 'carts',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserCarts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchUserCarts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.userCarts = action.payload;
+      })
+      .addCase(fetchUserCarts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload;
+      });
+  },
+});
+
+export default cartsSlice.reducer;
+
